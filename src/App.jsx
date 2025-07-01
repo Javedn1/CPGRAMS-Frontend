84d50693bcf7473387410a14d9a40067e1c92519
mohitkumar0831 <mohitkumar@netfotech.in>
2025-07-01 16:50:32 +0530

    added MyComplaints

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import react from 'react'
+// import react from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Auth from './pages/auth/Auth'
 import Home from './pages/home/Home'
@@ -7,6 +7,7 @@ import ForgotPassword from './components/auth/ForgotPassword'
 import ForgotPasswordOTP from './components/auth/ForgotPasswordOTP'
 import ChangePassword from './components/auth/ChangePassword'
 import AuthForgotPassword from './pages/auth/AuthForgotPassword'
+import MyGrievances from './pages/My Complaints/myGrievance';
 
 function App() {
 
@@ -18,6 +19,7 @@ function App() {
           <Route path='/auth' element={<Auth />} />
           <Route path='/forgotpassword' element={<AuthForgotPassword />} />
           <Route path='/grv' element={<GrievanceForm />} />
+          <Route path='/myGrievance' element={<MyGrievances />} />
         </Routes>
       </BrowserRouter>
 
