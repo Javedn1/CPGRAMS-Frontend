fd6ccb6eff5f3078277db611c1758e4265c2635c
Rishiraj-Pathak-2005 <rishiraj@netfotech.in>
2025-07-02 10:12:16 +0530

    initial commit from rishiraj

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,39 @@
-import react, { useState } from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Auth from "./pages/auth/Auth";
 import Home from "./pages/home/Home";
 import GrievanceForm from "./pages/GRV-form/GrivanceForm";
+import ForgotPassword from "./components/auth/ForgotPassword";
+import ForgotPasswordOTP from "./components/auth/ForgotPasswordOTP";
+import ChangePassword from "./components/auth/ChangePassword";
+import AuthForgotPassword from "./pages/auth/AuthForgotPassword";
+import MyGrievances from "./pages/My Complaints/myGrievance";
 import CitizenProfile from "./components/profile/citizen-profile";
-
-// import react from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Auth from './pages/auth/Auth'
-import Home from './pages/home/Home'
-import GrievanceForm from './pages/GRV-form/GrivanceForm'
-import ForgotPassword from './components/auth/ForgotPassword'
-import ForgotPasswordOTP from './components/auth/ForgotPasswordOTP'
-import ChangePassword from './components/auth/ChangePassword'
-import AuthForgotPassword from './pages/auth/AuthForgotPassword'
-import MyGrievances from './pages/My Complaints/myGrievance';
->>>>>>> 019c44e7704c1fe99b762990c451a585036687c3
 
 function App() {
   const [profileImage, setProfileImage] = useState(null);
+
   return (
     <>
       <BrowserRouter>
         <Routes>
-<<<<<<< HEAD
           <Route path="/" element={<Home />} />
           <Route path="/auth" element={<Auth />} />
+          <Route path="/forgotpassword" element={<AuthForgotPassword />} />
           <Route path="/grv" element={<GrievanceForm />} />
-          <Route path="/cp" element={<CitizenProfile profileImage={profileImage} setProfileImage={setProfileImage} />} />
+          <Route
+            path="/cp"
+            element={
+              <CitizenProfile
+                profileImage={profileImage}
+                setProfileImage={setProfileImage}
+              />
+            }
+          />
+          <Route path="/myGrievance" element={<MyGrievances />} />
         </Routes>
       </BrowserRouter>
-=======
-          <Route path='/' element={<Home />} />
-          <Route path='/auth' element={<Auth />} />
-          <Route path='/forgotpassword' element={<AuthForgotPassword />} />
-          <Route path='/grv' element={<GrievanceForm />} />
-          <Route path='/myGrievance' element={<MyGrievances />} />
-        </Routes>
-      </BrowserRouter>
-
->>>>>>> 019c44e7704c1fe99b762990c451a585036687c3
     </>
   );
 }
